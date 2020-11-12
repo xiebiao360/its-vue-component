99ab5856fb72d47f7aa1d4dd2319a8850c7a87c8
xiebiao <1404027855@qq.com>
2020-11-12 17:27:28 +0800

    commit

--- examples/main.js
+++ examples/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { Button } from 'element-ui';
-import ItsDialog from '../packages/index'
+import ItsComponents from '../packages/index'
 
 Vue.config.productionTip = false
 
 // Vue.prototype.$ELEMENT = { size: 'mini' };
 Vue.use(Button)
-Vue.use(ItsDialog)
+Vue.use(ItsComponents)
 
 new Vue({
   render: h => h(App),
